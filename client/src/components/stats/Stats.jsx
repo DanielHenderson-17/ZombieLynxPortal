c5765505eb149e0d2f7c171e85594187f5c9f3b5
Daniel Henderson <djhindu17@gmail.com>
2025-05-05 21:12:40 -0500

    fixed blank space with anime characters

--- client/src/components/stats/Stats.jsx
+++ client/src/components/stats/Stats.jsx
@@ -2,8 +2,9 @@ import "../../assets/styles/Stats.css";
 
 export default function Stats() {
   return (
-    <div className="stats-container">
-      <h1 className="pt-5">Stats Coming Soon</h1>
+    <div className="stats-container d-md-flex d-block justify-content-center align-items-center">
+      <img src="/public/Kibo.png" alt="" className="kibo" />
+      <h1 className="pt-md-5 pt-2 text-secondary">Stats Coming Soon!</h1>
     </div>
   );
 }
