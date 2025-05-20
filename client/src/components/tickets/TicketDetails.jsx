862074d8bf7a9276f03bf6129e75305e3e35f6ac
Daniel Henderson <djhindu17@gmail.com>
2025-05-20 18:26:23 -0500

    added close/restore button to ticket

--- client/src/components/tickets/TicketDetails.jsx
+++ client/src/components/tickets/TicketDetails.jsx
@@ -14,84 +14,103 @@ export default function TicketDetails({
   const assignedUser = ticket.assignedUsers[0];
 
   return (
-    <div className="col-12 mb-0 position-relative d-flex p-2 rounded-top align-items-center ticket-box shadow">
-      {/* Div 1 - Game Image */}
-      <div className="me-2 d-flex align-items-center justify-content-center">
-        <img
-          src={getGameImage(ticket.game)}
-          alt={ticket.game}
-          className="rounded"
-          style={{ width: "100px", height: "100px", objectFit: "cover" }}
-        />
-      </div>
-
-      {/* Div 2 - Info box */}
-      <div className="flex-grow-1 mb-0">
-        {/* Top Row - User + category + date */}
-        <div className="d-flex align-items-center flex-wrap mb-0">
+    <div className=" ticket-box shadow">
+      <div className="col-12 mb-0 position-relative d-flex p-2 rounded-top align-items-center">
+        {/* Div 1 - Game Image */}
+        <div className="me-2 d-flex align-items-center justify-content-center">
           <img
-            src={
-              assignedUser?.zlgMember?.discordImgUrl ||
-              "https://cdn.discordapp.com/embed/avatars/0.png"
-            }
-            alt="Discord Avatar"
-            className="rounded-circle me-2"
-            style={{ width: "30px", height: "30px" }}
+            src={getGameImage(ticket.game)}
+            alt={ticket.game}
+            className="rounded"
+            style={{ width: "100px", height: "100px", objectFit: "cover" }}
           />
-          <span className="me-2">
-            {formatDiscordName(assignedUser?.zlgMember?.discordName) ||
-              `${assignedUser?.firstName} ${assignedUser?.lastName}`}
-          </span>
-          <span className="me-2 text-secondary">
-            -{" "}
-            <span
-              dangerouslySetInnerHTML={{
-                __html: categoryFormatter(ticket.category),
-              }}
-            />
-          </span>
-          <span className="text-secondary single-ticket-date d-none d-md-inline">
-            - {formatLongDateTime(ticket.updatedAt)}
-          </span>
         </div>
 
-        {/* Subject */}
-        <h5 className="text-white m-0 text-start">
-          {truncateText(ticket.subject, 25)}
-        </h5>
+        {/* Div 2 - Info box */}
+        <div className="flex-grow-1 mb-0">
+          {/* Top Row - User + category + date */}
+          <div className="d-flex align-items-center flex-wrap mb-0">
+            <img
+              src={
+                assignedUser?.zlgMember?.discordImgUrl ||
+                "https://cdn.discordapp.com/embed/avatars/0.png"
+              }
+              alt="Discord Avatar"
+              className="rounded-circle me-2"
+              style={{ width: "30px", height: "30px" }}
+            />
+            <span className="me-2">
+              {formatDiscordName(assignedUser?.zlgMember?.discordName) ||
+                `${assignedUser?.firstName} ${assignedUser?.lastName}`}
+            </span>
+            <span className="me-2 text-secondary">
+              -{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: categoryFormatter(ticket.category),
+                }}
+              />
+            </span>
+            <span className="text-secondary single-ticket-date d-none d-md-inline">
+              - {formatLongDateTime(ticket.updatedAt)}
+            </span>
+          </div>
 
-        {/* Description */}
-        <p className="text-light mb-0 mt-1 text-start">
-          {truncateText(ticket.description, 30)}
-        </p>
-        {/* Server */}
-        <p className="text-white mb-0 mt-1 text-start">
-          {truncateText(ticket.server, 30)}
-        </p>
+          {/* Subject */}
+          <h5 className="text-white m-0 text-start">
+            {truncateText(ticket.subject, 25)}
+          </h5>
+
+          {/* Description */}
+          <p className="text-light mb-0 mt-1 text-start">
+            {truncateText(ticket.description, 30)}
+          </p>
+          {/* Server */}
+          <p className="text-white mb-0 mt-1 text-start">
+            {truncateText(ticket.server, 30)}
+          </p>
+        </div>
+
+        {/* Div 3 - Close / Restore */}
+        <div className="d-md-flex d-none flex-column justify-content-between align-items-end pt-0">
+          {ticket.status === "Open" ? (
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={() => handleCloseTicket(ticketId, navigate)}
+            >
+              Close <i className="bi bi-x-circle ms-1"></i>
+            </button>
+          ) : (
+            <button
+              className="btn btn-primary btn-sm"
+              onClick={() => handleRestoreTicket(ticketId, navigate)}
+            >
+              Restore <i className="bi bi-arrow-counterclockwise ms-1"></i>
+            </button>
+          )}
+
+          {/* Absolute positioned Ticket ID */}
+          <i className="text-secondary">
+            <small>Ticket #{ticket.id}</small>
+          </i>
+        </div>
       </div>
-
-      {/* Div 3 - Close / Restore */}
-      <div className="d-md-flex d-none flex-column justify-content-between align-items-end ms-3 pt-3">
+      <div className="d-md-none d-block flex-column justify-content-between align-items-center col-12">
         {ticket.status === "Open" ? (
           <button
-            className="btn btn-danger btn-sm"
+            className="btn btn-danger btn-sm col-12"
             onClick={() => handleCloseTicket(ticketId, navigate)}
           >
             Close <i className="bi bi-x-circle ms-1"></i>
           </button>
         ) : (
           <button
-            className="btn btn-primary btn-sm"
+            className="btn btn-primary btn-sm col-12"
             onClick={() => handleRestoreTicket(ticketId, navigate)}
           >
             Restore <i className="bi bi-arrow-counterclockwise ms-1"></i>
           </button>
         )}
-
-        {/* Absolute positioned Ticket ID */}
-        <i className="text-secondary">
-          <small>Ticket #{ticket.id}</small>
-        </i>
       </div>
     </div>
   );
