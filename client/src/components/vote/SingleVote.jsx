bea7e8d11db4e9c94c50243e68c40cf133831323
Daniel Henderson <djhindu17@gmail.com>
2025-05-24 22:26:01 -0500

    minor edits to doughnut chart

--- client/src/components/vote/SingleVote.jsx
+++ client/src/components/vote/SingleVote.jsx
@@ -83,16 +83,16 @@ export default function SingleVote() {
 
   return (
     <div
-      className={`fade-container pb-0 pt-3 text-white d-flex align-items-center mx-auto col-md-10 col-12 ${
+      className={`fade-container pb-0 pt-3 text-white d-flex align-items-center mx-auto col-md-10 col-12  ${
         isVisible ? "fade-in" : "fade-start"
       } pt-md-5 pt-0 px-2`}
     >
-      <div className="px-2 pt-md-5 mt-md-0 mt-0 rounded-3 col-12 mx-auto bg-md-dark d-md-flex d-block align-items-center">
+      <div className="px-0 pt-md-5 mt-md-0 mt-0 rounded-3 col-12 mx-auto bg-md-dark d-md-flex d-block align-items-center px-md-5">
         <div
-          className="d-flex flex-row mb-md-5 mb-4 vote-header col-12 col-md-6 rounded-3 bg-dark shadow"
+          className="d-flex flex-row mb-md-5 mb-md-4 mb-1 vote-header col-12 col-md-6 rounded-3 bg-dark shadow"
           style={{ gap: ".2rem" }}
         >
-          <div style={{ flex: 1 }} className="text-center p-2 vote-img">
+          <div className="text-center p-2 p-md-0 vote-img">
             <img
               src={getVoteGameImage(vote.game)}
               alt={vote.game}
@@ -109,7 +109,7 @@ export default function SingleVote() {
             </p>
             {vote.userVote !== null && (
               <div className="d-none d-md-block mt-2 align-self-bottom">
-                <p className="fw-bold text-white">
+                <p className="text-white mb-0">
                   Your vote:{" "}
                   <span
                     className={vote.userVote ? "text-success" : "text-danger"}
@@ -117,14 +117,36 @@ export default function SingleVote() {
                     {vote.userVote ? "✅" : "❌"}
                   </span>
                 </p>
+                <p className="mt-1 p-0">
+                  Total Votes :{" "}
+                  <span className="text-success fw-bold">{votesFor}</span> /{" "}
+                  <span className="text-danger fw-bold">{votesAgainst}</span>{" "}
+                  <span className="fw-bold">({totalVotes})</span>
+                </p>
               </div>
             )}
           </div>
         </div>
+        {vote.userVote !== null && (
+          <div className="d-flex d-md-none mt-0 justify-content-between align-items-start mx-1">
+            <p className="text-white mb-0 mt-1">
+              Your vote:{" "}
+              <span className={vote.userVote ? "text-success" : "text-danger"}>
+                {vote.userVote ? "✅" : "❌"}
+              </span>
+            </p>
+            <p className="mt-1 p-0">
+              Total Votes :{" "}
+              <span className="text-success fw-bold">{votesFor}</span> /{" "}
+              <span className="text-danger fw-bold">{votesAgainst}</span>{" "}
+              <span className="fw-bold">({totalVotes})</span>
+            </p>
+          </div>
+        )}
         <div
-          className="position-relative pt-2"
+          className="position-relative pt-md-2 pt-0"
           style={{
-            width: "100%",
+            width: "90%",
             maxWidth: "450px",
             margin: "0 auto",
           }}
