e4ed0ce678005f6d60a11cb77b2f5c3e1a49ad72
Daniel Henderson <djhindu17@gmail.com>
2025-06-07 21:49:22 -0500

    removed images from open tickets and stats

--- client/src/components/stats/ArkSEStats.jsx
+++ client/src/components/stats/ArkSEStats.jsx
@@ -15,7 +15,6 @@ export default function ArkSEStats() {
         isVisible ? "fade-in" : "fade-start"
       }`}
     >
-      <img src="/images/Kibo.png" alt="" className="kibo" />
       <h1 className="pt-md-5 pt-2 text-secondary">Ark:SE Stats Coming Soon!</h1>
     </div>
   );
