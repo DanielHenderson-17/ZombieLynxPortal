5026b21cc206fbf1e7f8cba49d5bb8d3bb89fca8
Daniel Henderson <djhindu17@gmail.com>
2026-02-07 16:50:22 -0600

    cert problems

--- Program.cs
+++ Program.cs
@@ -367,7 +367,6 @@ app.MapControllers();
 // ✅ Bind to all network interfaces
 app.Urls.Add("http://0.0.0.0:5001");
 app.Urls.Add("http://0.0.0.0:5000");
-app.Urls.Add("https://localhost:5001");
 
 
 // ✅ Start <GameName>PointsSyncService after app startup
